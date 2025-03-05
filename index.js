939d731841f3e6419802af17393552b8952a7907
Korir Carlos <carlkorir226@gmail.com>
2025-03-05 11:53:51 +0300

    adds functon lab

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(start){
+    if (start> 42){
+        return start - 42
+    }
+    else if( start< 42){
+        return 42 - start
+    }
+}
+
+function distanceFromHqInFeet(start){
+    if (start > 42){
+        return ((start - 42 )* 264)
+    }
+    else if (start < 42){
+        return ((42 - start) * 264)
+    }
+}
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        return((destination - start) * 264)
+    }
+    else if (start > destination){
+        return((start - destination) * 264)
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let dist =  Math.abs (start - destination)
+    if ((dist *264) <= 400){
+        return 0
+    }
+    else if (((dist *264) > 400) && ((dist *264) < 2000)){
+        return (((dist * 264)-400) * 0.02)
+    }
+    else if (((dist * 264) > 2000) && ((dist*264)<2500)){
+        return 25
+    }
+    else{ 
+    return 'cannot travel that far'
+    }
+}
